64a5dc82d180d3409b142ce7b126013ca63dc936
rodrigomrodovalho <rodrigomrodovalho@gmail.com>
2017-06-03 12:24:43 -0300

    Create base class for UseCase Adds the dependency of Rx Java

--- domain/src/main/java/br/com/stone/store/domain/MyClass.java
+++
@@ -1,4 +0,0 @@
-package br.com.stone.store.domain;
-
-public class MyClass {
-}
---
+++ domain/src/main/java/br/com/stone/store/domain/base/UseCase.java
@@ -0,0 +1,27 @@
+package br.com.stone.store.domain.base;
+
+import io.reactivex.Observable;
+
+/**
+ * Created by rrodovalho on 03/06/17.
+ */
+
+public abstract class UseCase<RV extends UseCase.RequestValues,T> {
+
+    private RV mRequestValues;
+
+    public void setRequestValues(RV requestValues) {
+        mRequestValues = requestValues;
+    }
+
+    public Observable<T> run() {
+        return executeUseCase(mRequestValues);
+    }
+
+    protected abstract Observable<T> executeUseCase(RV requestValues);
+
+
+    public interface RequestValues {
+    }
+
+}
