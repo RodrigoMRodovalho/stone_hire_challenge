06ed9fa8914360caab77f43807085e0652a6f01d
rodrigomrodovalho <rodrigomrodovalho@gmail.com>
2017-06-03 22:43:16 -0300

    -Create retrofit endpoints declaration through StoreRestService interface

--- data/src/main/java/br/com/stone/store/data/repository/remote/StoreRestService.java
+++ data/src/main/java/br/com/stone/store/data/repository/remote/StoreRestService.java
@@ -1,8 +1,27 @@
 package br.com.stone.store.data.repository.remote;
 
+import java.util.List;
+
+import br.com.stone.store.data.model.CheckoutModel;
+import br.com.stone.store.data.model.StoreItem;
+import io.reactivex.Observable;
+import retrofit2.Response;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Url;
+
 /**
  * Created by rrodovalho on 03/06/17.
  */
 
 public interface StoreRestService {
+
+    @GET
+    Observable<List<StoreItem>> getStoreItems(@Url String storeItensUrl);
+
+    @POST
+    Observable<Response<Void>> finishCheckout(@Url String storeCheckoutUrl, @Body CheckoutModel checkoutModel);
+
+
 }
